b853796e58dda4bee3f390eeee8968ae37f2005a
Himanshu Kumar <himanshu.kumar2@outlook.com>
2022-05-25 15:27:05 -0700

    completed color flip by selected combo

---
+++ app.js
@@ -0,0 +1,14 @@
+const colors = ["violet", "#F2AF29", "rgba(89,63,98)", "orange"];
+const button = document.getElementById("button");
+const color = document.querySelector(".color");
+
+button.addEventListener("click", function () {
+    const randomValue = getArrayIndex();
+    console.log("The randon vlaue generated is:",randomValue);
+    document.body.style.backgroundColor = colors[randomValue];
+    color.textContent = colors[randomValue];
+  });
+  
+  function getArrayIndex() {
+    return Math.floor(Math.random() * colors.length);
+  }
